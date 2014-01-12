956c82d11d11a8ebb51944e51bc96a0459ff405e
Jeff Cross <middlefloor@gmail.com>
2014-01-11 23:10:10 -0800

    Add login directive + angular strap navbar

--- app/public/home/home-controller.js
+++ app/public/home/home-controller.js
@@ -1,4 +1,4 @@
 angular.module('nutritiousApp').
-  controller('DashboardController', function() {
+  controller('HomeController', function() {
 
   });
--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,25 +1,16 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('nutritiousApp', [
-  'ngRoute'
+  'dpdUser',
+  'ngRoute',
+  'mgcrea.ngStrap'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
     templateUrl: 'home/home.html',
     controller: 'HomeController',
     controllerAs: 'homeCtrl'
-  }).
-  when('/users/login', {
-    templateUrl: 'user/login/login.html',
-    controller: 'UserLoginController',
-    controllerAs: 'loginCtrl'
-  }).
-  when('/users/register', {
-    templateUrl: 'user/register/register.html',
-    controller: 'UserRegisterController',
-    controllerAs: 'registerCtrl'
   }).
   when('/users/detail/:id', {
     templateUrl: 'user/detail/detail.html',
